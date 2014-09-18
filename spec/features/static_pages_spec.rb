ca0a3811a53c9b8bb417a5108a7138358818d0d2
Mark E. DeYoung <mark.deyoung@usma.edu>
2014-09-17 20:49:36 -0400

    modifed routes and route tests

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe "StaticPages", :type => :request do
   describe "GET /static_pages" do
     it "home page exists" do
-	get static_pages_home_path
+	get root_path
 	expect(response).to have_http_status(200)
 	end
 	
 	it "help page exists" do
-	get static_pages_help_path
+	get help_path
 	expect(response).to have_http_status(200)
 	end
 	
 	it "about page exists" do
-	get static_pages_about_path
+	get about_path
 	expect(response).to have_http_status(200)
 	end
   end
