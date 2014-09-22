3b7cb1243431da43417158bd6ef96c97e9e1c9d2
Mark E. DeYoung <mark.deyoung@usma.edu>
2014-09-22 14:24:56 -0400

    Added signup form test cases

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -12,20 +12,10 @@ end
  describe "User pages" do
 
   let(:base_title) { "USMA CS450" }
-	
-  describe "Signup page" do
-	before {visit signup_path}
-	
-    it "should have the h1 'USMA CS450 Sample App'" do
-       expect(page).to have_selector('h1', :text => 'Sign Up')
-    end
-    
-   it "should have the title 'Sign Up'" do
-		expect(page).to have_title "#{base_title} | Sign Up"
-   end
-  end
 
-  subject {page}
+  subject {page}	
+
+  
   describe "profile page" do
 	let(:user) {FactoryGirl.create(:user)}
 	before { visit user_path(user) }
@@ -34,5 +24,38 @@ end
 	it{ expect(page).to have_title user.name}
   end
 
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title "#{base_title} | Sign Up" }
+  end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    #Hartl Listing 7.16 Some good basic tests for signing up users
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 
 end
