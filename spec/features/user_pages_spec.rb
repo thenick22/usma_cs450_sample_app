665f6aabbb51c0cbe5c7d28d4394efae4cc1d8fe
Mark E. DeYoung <mark.deyoung@usma.edu>
2014-09-22 07:07:58 -0400

    Added FactoryGirl test

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -11,7 +11,7 @@ end
  
  describe "User pages" do
 
-	let(:base_title) { "USMA CS450" }
+  let(:base_title) { "USMA CS450" }
 	
   describe "Signup page" do
 	before {visit signup_path}
@@ -19,9 +19,19 @@ end
     it "should have the h1 'USMA CS450 Sample App'" do
        expect(page).to have_selector('h1', :text => 'Sign Up')
     end
-	it "should have the title 'Sign Up'" do
+    
+   it "should have the title 'Sign Up'" do
 		expect(page).to have_title "#{base_title} | Sign Up"
-	end
+   end
+  end
+
+  subject {page}
+  describe "profile page" do
+	let(:user) {FactoryGirl.create(:user)}
+	before { visit user_path(user) }
+
+	it{ should have_selector('h1', text: user.name) }
+	it{ expect(page).to have_title user.name}
   end
 
 
